0472e2a92e465cc187bd056aceb033978f88c92f
Mayank Rana <mayankranax1@gmail.com>
2022-04-14 16:25:55 +0530

    commit

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,6 +1,16 @@
 const baseURL = "https://api.github.com";
 
 export async function getUser(userName: string): Promise<USER> {
+  // return {
+  //   avatar_url: "https://avatars.githubusercontent.com/u/58820001?v=4",
+  //   html_url: "https://github.com/octocat",
+  //   name: "monalisa octocat",
+  //   location: "San Francisco",
+  //   bio: "There once was...",
+  //   twitter_username: "monatheoctocat",
+  //   public_repos: 2,
+  // };
+
   const result = await fetch(`${baseURL}/users/${userName}`, {
     headers: new Headers({ Accept: "application/vnd.github.v3+json" }),
   });
@@ -16,6 +26,79 @@ export async function getRepositories(
   userName: string,
   page: number
 ): Promise<REPOS> {
+  // return [
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   },
+  //   {
+  //     name: "HelloWorlddsfjkndkjsfhjkskk kjdfkjsfkj",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hellodskfjsdkfhskjdhfkjsdkjdjshfkjjsdkjfskdjfskjfdkjadnfkasafhkjsadfkihkjudsbfbdsfksdjfkjdsfksldjkfljsdfjlsdkjfklsdjfklsjdflksjdklfjkldjflk",
+  //   },
+  //   {
+  //     name: "Hello-World",
+  //     html_url: "https://github.com/octocat/Hello-World",
+  //     description: "Hello",
+  //   }
+  // ];
+
   const result = await fetch(
     `${baseURL}/users/${userName}/repos?page=${page}&per_page=6`,
     {
